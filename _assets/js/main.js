bbd7285520f1a88a51134baf9b6458f9752035af
Toby Trembath <toby@twobyte.com>
2018-06-22 18:04:54 +0100

    :construction: updated content, formatting still to complete

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -5,3 +5,9 @@
 // img.style = "background: #2B3A42; padding: 2px"
 // img.width = 64
 // document.body.appendChild(img)
+
+const supportsMixBlendMode = window.getComputedStyle(document.body).mixBlendMode;
+if (supportsMixBlendMode) {
+  const root = document.documentElement;
+  root.className += ' has-blendmode';
+}
